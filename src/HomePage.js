610db768b1ced3b2dd55055412b8194bdbc85a7a
lyt <124516380+X-yahya@users.noreply.github.com>
2024-02-28 18:11:01 +0200

    update

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,8 +5,8 @@ export default function loadHome(selector) {
     .addAttributes({class: "tab"})
     .addChild(new createElem("h1").setInnerText("Welcome to lyt Cofee Shop"))
     .addChild(
-      new createElem("img").addAttributes({id:"home-img"})
-    )
+      new createElem("p").setInnerText("Nestled beneath a canopy of twinkling fairy lights, this quaint cafe exudes warmth and charm. Aromas of freshly brewed coffee mingle with the tantalizing scent of pastries, drawing patrons into a cozy haven of rustic décor and soft jazz melodies. With plush seating nooks and sunlit corners, it's a sanctuary for indulging in culinary delights and heartfelt conversations."))
+    .addChild(new createElem("img").addAttributes({id:"home-img"}))
 
   selector.appendChild(homeElement.load());
 }
